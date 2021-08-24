8ece5bdb27e4ec2a0f52c64617eea396b074b945
Matt Watson <mattwat@gmail.com>
2021-08-24 14:26:30 -0400

    get basic graph returned from placeholder query

--- src/components/result-modal/tabs/tranql-tab.js
+++ src/components/result-modal/tabs/tranql-tab.js
@@ -1,9 +1,10 @@
 import { Fragment, useState } from 'react'
 import axios from 'axios'
-import { Button, Divider, Input } from 'antd'
+import { Button, Divider, Input, Spin } from 'antd'
 import ForceGraph2D from 'react-force-graph-2d'
 import { useHelxSearch } from '../../'
 import { RocketOutlined as QueryIcon } from '@ant-design/icons'
+import { SizeMe } from 'react-sizeme'
 import './tranql-tab.css'
 
 const { TextArea } = Input
@@ -13,10 +14,12 @@ export const TranQLTab = ({ result }) => {
   const [tranqlQuery, setTranqlQuery] = useState(`select chemical_substance->gene->disease
   from "/graph/gamma/quick"
  where disease="${ result.name }"`)
+  const [busy,setBusy] = useState(false)
   const [nodes, setNodes] = useState([])
   const [edges, setEdges] = useState([])
 
   const fetchGraph = async () => {
+    setBusy(true)
     const headers = {
       'Content-Type': 'text/plain',
     }
@@ -31,32 +34,35 @@ export const TranQLTab = ({ result }) => {
         console.log('no data')
         return
       }
-      console.log(data)
       setNodes(data.knowledge_graph.nodes)
       setEdges(data.knowledge_graph.edges.map(edge => ({ source: edge.source_id, target: edge.target_id })))
     } catch (error) {
       console.error(error)
+    } finally {
+      setBusy(false)
     }
   }
 
   return (
     <Fragment>
       <TextArea className="tranql-query-textarea" value={ tranqlQuery } rows="3" />
-      <Button onClick={ fetchGraph } type="default" block icon={ <QueryIcon rotate={ 90 } style={{ padding: '0 1rem 0 1rem' }} /> } />
+      <Button onClick={ fetchGraph } type="primary" ghost block icon={ <QueryIcon rotate={ 90 } style={{ padding: '0 1rem 0 1rem' }} /> } loading={ busy } />
       <Divider />
-      {
-        nodes && edges && (
-          <ForceGraph2D
-            height={ 400 }
-            width={ 400 }
-            graphData={{ nodes: nodes, links: edges }}
-            nodeLabel="id"
-            linkColor={ () => 'rgba(0,0,0,0.2)' }
-            enablePointerInteraction={ true }
-            d3VelocityDecay={ 0.25 }
-          />
-        )
-      }
+      <SizeMe>
+        {
+          ({ size }) => (
+            <ForceGraph2D
+              height={ 600 }
+              width={ size.width }
+              graphData={{ nodes: nodes, links: edges }}
+              nodeLabel="id"
+              linkColor={ () => 'rgba(0,0,0,0.2)' }
+              enablePointerInteraction={ true }
+              d3VelocityDecay={ 0.25 }
+            />
+          )
+        }
+      </SizeMe>
     </Fragment>
   )
 }
