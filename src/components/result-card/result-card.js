01d99662385a6b8a903fcdfaba3359ca88e34f45
Matt Watson <mattwat@gmail.com>
2021-09-22 19:41:20 -0400

    use icons in result card tabs

--- src/components/result-card/result-card.js
+++ src/components/result-card/result-card.js
@@ -7,6 +7,14 @@ import { KnowledgeGraphs, useHelxSearch } from '../'
 import { OverviewTab } from './overview-tab'
 import { StudiesTab } from './studies-tab'
 import { KnowledgeGraphsTab } from './knowledge-graphs-tab'
+import {
+  InfoCircleOutlined as OverviewIcon,
+  BookOutlined as StudiesIcon,
+  ShareAltOutlined as KnowledgeGraphsIcon,
+  CodeOutlined as TranQLIcon,
+  QuestionCircleOutlined as QuestionGraphsIcon,
+  QuestionOutlined as AnswerViewerIcon,
+} from '@ant-design/icons'
 import './result-card.css'
 
 const { Meta } = Card
@@ -17,12 +25,24 @@ export const SearchResultCard = ({ index, result, openModalHandler }) => {
   const [currentTab, setCurrentTab] = useState('overview')
 
   const tabs = {
-    'overview': { title: 'Overview',         content: <OverviewTab result={ result } /> },
-    'studies':  { title: `Studies`,          content: <StudiesTab result={ result } /> },
-    'kgs':      { title: `Knowledge Graphs`, content: <KnowledgeGraphsTab result={ result } /> },
+    'overview': { title: 'Overview',         icon: <OverviewIcon />,         content: <OverviewTab result={ result } /> },
+    'studies':  { title: `Studies`,          icon: <StudiesIcon />,          content: <StudiesTab result={ result } /> },
   }
 
-  const tabList = Object.keys(tabs).map(key => tabs[key].content ? ({ key, tab: tabs[key].title }) : null).filter(tab => tab !== null)
+  const tabList = Object.keys(tabs).map(key => {
+    if (!tabs[key].content) {
+      return null
+    }
+    return {
+      key,
+      tab: (
+        <span>
+          <span className="tab-icon">{ tabs[key].icon }</span>
+          <span className="tab-name">{ tabs[key].title }</span>
+        </span>
+      ),
+    }
+  }).filter(tab => tab !== null)
   const tabContents = Object.keys(tabs).reduce((obj, key) => tabs[key].content ? ({ ...obj, [key]: tabs[key].content }) : obj, {})
 
   return (
