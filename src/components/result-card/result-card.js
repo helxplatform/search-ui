d880adfa2705130de91eec60c3363a6784102af7
Matt Watson <mattwat@gmail.com>
2021-09-23 20:16:13 -0400

    (idea) simplify result cards to only overview
    remove now-unused imports

--- src/components/result-card/result-card.js
+++ src/components/result-card/result-card.js
@@ -1,63 +1,22 @@
 import { Fragment, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { Link } from '../link'
-import { Button, Card, List, Space, Tag, Typography } from 'antd'
+import { Button, Card } from 'antd'
 import { ExpandOutlined as ViewIcon } from '@ant-design/icons'
-import { KnowledgeGraphs, useHelxSearch } from '../'
-import { OverviewTab } from './overview-tab'
-import { StudiesTab } from './studies-tab'
-import { KnowledgeGraphsTab } from './knowledge-graphs-tab'
-import {
-  InfoCircleOutlined as OverviewIcon,
-  BookOutlined as StudiesIcon,
-  ShareAltOutlined as KnowledgeGraphsIcon,
-  CodeOutlined as TranQLIcon,
-  QuestionCircleOutlined as QuestionGraphsIcon,
-  QuestionOutlined as AnswerViewerIcon,
-} from '@ant-design/icons'
-import './result-card.css'
+import './result-card.scss'
 
 const { Meta } = Card
-const { CheckableTag: CheckableFacet } = Tag
-const { Text } = Typography
 
 export const SearchResultCard = ({ index, result, openModalHandler }) => {
-  const [currentTab, setCurrentTab] = useState('overview')
-
-  const tabs = {
-    'overview': { title: 'Overview',         icon: <OverviewIcon />,         content: <OverviewTab result={ result } /> },
-    'studies':  { title: `Studies`,          icon: <StudiesIcon />,          content: <StudiesTab result={ result } /> },
-  }
-
-  const tabList = Object.keys(tabs).map(key => {
-    if (!tabs[key].content) {
-      return null
-    }
-    return {
-      key,
-      tab: (
-        <span>
-          <span className="tab-icon">{ tabs[key].icon }</span>
-          <span className="tab-name">{ tabs[key].title }</span>
-        </span>
-      ),
-    }
-  }).filter(tab => tab !== null)
-  const tabContents = Object.keys(tabs).reduce((obj, key) => tabs[key].content ? ({ ...obj, [key]: tabs[key].content }) : obj, {})
-
   return (
     <Fragment>
       <Card
         className="result-card"
         title={ result.name }
-        tabList={ tabList }
-        tabProps={{ size: 'small' }}
-        activeTabKey={ currentTab }
-        onTabChange={ key => setCurrentTab(key) }
+        onClick={ openModalHandler }
         extra={ <Button onClick={ openModalHandler } type="link" icon={ <ViewIcon /> } /> }
         actions={ [<br />] }
       >
-        { tabContents[currentTab] }
+        <Meta description={ result.description } className="description" />
       </Card>
    </Fragment>
   )
